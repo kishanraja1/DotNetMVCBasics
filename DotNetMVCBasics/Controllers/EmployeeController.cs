be3c0b3fa9eca30492976e793ced75f12e61c069
Kishan Raja <kraja@experientgroup.com>
2022-08-24 09:53:40 -0400

    push

---
+++ DotNetMVCBasics/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using DotNetMVCBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMVCBasics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> employeelist = new List<Employee>();
+            Employee employee = new Employee();
+            employee.ID = 1;
+            employee.Name = "kishan";
+            employee.Salary = 5;
+            employeelist.Add(employee);
+            Employee employee2 = new Employee();
+            employee2.ID = 2;
+            employee2.Name = "Raja";
+            employee2.Salary = 10;
+            employeelist.Add(employee2);
+            return View(employeelist);
+        }
+
+        public ActionResult AddEmployee()
+        {
+            Employee emp = new Employee();
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Employee employee)
+        {
+            return View();
+        }
+
+        public ActionResult UpdateEmployee()
+        {
+            Employee employee = new Employee();
+            employee.ID = 1;
+            employee.Name = "Vi";
+            employee.Salary = 4;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UpdateEmployee(Employee employee)
+        {
+            return View(employee);
+        }
+    }
+}
---
+++ DotNetMVCBasics/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMVCBasics.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Redirect("https://oakacademy.uk");
+        }
+
+        public ActionResult FirstAction()
+        {
+            return View();
+        }
+
+        public ActionResult GoToProduct()
+        {
+            //Operations
+            // return RedirectToAction("Product/AddProduct")
+            //return RedirectToAction("Index");
+            return RedirectToAction("AddProduct", "Product");
+        }
+
+        public ActionResult GotoUpdateProduct()
+        {
+            return RedirectToAction("UpdateProduct", "Product", new { ID = 20 });
+        }
+
+
+    }
+}
